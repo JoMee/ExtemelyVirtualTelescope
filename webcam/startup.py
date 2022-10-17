ac2293bb030e063893928513ad598f21000407e5
Jonas Meier <jonas.meier@inf.ethz.ch>
2022-10-17 11:07:24 +0900

    this time, i think i really fixed the previous bug

--- webcam/startup.py
+++ webcam/startup.py
@@ -2,7 +2,4 @@ import os
 import subprocess
 import sys
 
-os.system("Xvfb :1 &")
-os.environ['DISPLAY'] = ":1" # visible in this process + all children
-
 os.system("cd src && flask run --host=0.0.0.0")
